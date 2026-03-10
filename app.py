1044c4bee7de0ecd01ecb1ee8155a38e33d28f1b
Bigyan Bashyal <bigyan.bashyal>
2026-03-09 22:55:58 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#read data from CSV file into a dataframe
+
+#df = pd.read_csv("/Users/binga17/Desktop/naya/Pandas/sales_data_sample.csv", encoding="latin1")
+
+#df = pd.read_excel("/Users/binga17/Desktop/naya/Pandas/SampleSuperstore.xlsx")
+
+print(df)
---
+++ in.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_json("simple_Data.json")
+
+
+print("Displaiyng the info of dataset")
